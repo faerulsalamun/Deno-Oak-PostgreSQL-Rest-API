1bed90776a8e1742dd8df68a7f761cb03ea11be9
Faerul Salamun <faerulsalamun@gmail.com>
2020-06-01 20:55:23 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,42 +1,4 @@
--- ----------------------------
--- Sequence structure for users_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."users_id_seq";
-CREATE SEQUENCE "public"."users_id_seq"
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 2147483647
-START 1
-CACHE 1;
-ALTER SEQUENCE "public"."users_id_seq" OWNER TO "faerulsalamun";
-
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS "public"."users";
-CREATE TABLE "public"."users" (
-  "id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-  "name" varchar(50) COLLATE "pg_catalog"."default" NOT NULL
-)
-;
-ALTER TABLE "public"."users" OWNER TO "faerulsalamun";
-
--- ----------------------------
--- Records of users
--- ----------------------------
-BEGIN;
-INSERT INTO "public"."users" VALUES (1, 'faerul');
-INSERT INTO "public"."users" VALUES (2, 'salamun');
-COMMIT;
-
--- ----------------------------
--- Alter sequences owned by
--- ----------------------------
-ALTER SEQUENCE "public"."users_id_seq"
-OWNED BY "public"."users"."id";
-SELECT setval('"public"."users_id_seq"', 2, true);
-
--- ----------------------------
--- Primary Key structure for table users
--- ----------------------------
-ALTER TABLE "public"."users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+CREATE TABLE "users" (
+  "id" serial NOT NULL,
+  "name" varchar(255) NOT NULL
+);
